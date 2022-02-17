ef47758debfadb51a8d957365a66da7d3e5c714f
Parag Bafna <parag.bafna@geoedge.com>
2022-02-17 09:43:23 +0530

    Add AppHarbrSDK 1.3.1

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AppHarbrSDK",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "AppHarbrSDK",
+            targets: ["AppHarbrSDK"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "AppHarbrSDK",
+            url: "https://nexus.geoedge.com/repository/raw-ios/1.3.1/AH-SDK-iOS-1.3.1.zip",
+            checksum: "4b529437fe9a65cae7d14fac362c6418867ef404ebcfbc9ff2029dab351b8ef5"
+        ),
+    ]
+)
---
+++ README.md
@@ -0,0 +1,4 @@
+# AppHarbrSDK
+
+Secure Ad Quality and Refine User Experience.
+Copyright 2022 GeoEdge Ltd.
