cdf62ec05c801c752285c609f5ba1eefd80fa04f
Parag Bafna <parag.bafna@geoedge.com>
2023-11-23 21:44:17 +0530

    Update AppHarbrSDK to 1.9.1

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AppHarbrSDK",
-            url: "https://nexus.geoedge.com/repository/raw-ios/1.9.0/AH-SDK-iOS-1.9.0.zip",
-            checksum: "41c9b1052918819916bd79fdcc200c326014e9057f1a1931d74d5ab8cb37aa91"
+            url: "https://nexus.geoedge.com/repository/raw-ios/1.9.1/AH-SDK-iOS-1.9.1.zip",
+            checksum: "6981332083470e9496b6e1de445febb0d7a7b69b16af990086d2fb09a3230ce6"
         ),
     ]
 )
