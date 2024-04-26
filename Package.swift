dee6392791c4b730e346cc064d58e7f4b5e35ee7
Parag Bafna <parag.bafna@geoedge.com>
2024-04-26 15:09:10 +0530

    Update AppHarbrSDK to 1.14.1

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AppHarbrSDK",
-            url: "https://geoedge-rum.s3.amazonaws.com/sdk/released_versions/ios-deployment/AH-SDK-iOS-1.14.0.zip",
-            checksum: "ac16edc1780b4e4a6c085cd61738fc7edd8ff4f623ea868e4f74a7cc117a7d3d"
+            url: "https://geoedge-rum.s3.amazonaws.com/sdk/released_versions/ios-deployment/AH-SDK-iOS-1.14.1.zip",
+            checksum: "ba0d71b5fa25e810b1c274801b19eb5ce0937fbd979df698aa39065577cb709f"
         ),
     ]
 )
